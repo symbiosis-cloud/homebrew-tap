5d188351c7dbe6d5f5931dcaabb9920e9d467a0f
Stephen <stephen@tca0.nl>
2023-01-04 15:01:12 +0100

    updated formula

--- Formula/sym.rb
+++ Formula/sym.rb
@@ -1,8 +1,8 @@
 class Sym < Formula
   desc "Symbiosis Managed Kubernetes CLI tool"
   homepage "https://github.com/symbiosis-cloud/cli"
-  url "https://github.com/symbiosis-cloud/cli/archive/refs/tags/v0.0.3.tar.gz"
-  sha256 "b9d385b0b7685b6208bcda8bee6691265867d70b4c531761ba559076f0a93885"
+  url "https://github.com/symbiosis-cloud/cli/archive/refs/tags/v0.0.6.tar.gz"
+  sha256 "1aa99c2afa20d4f8855d0c4d01dafc7437bf0cbf5191fbbbcc1836689b0f12fe"
   license "Apache-2.0"
   head "https://github.com/symbiosis-cloud/cli.git", branch: "main"
 
@@ -20,4 +20,3 @@ class Sym < Formula
     assert_match "Symbiosis CLI version v#{version}", shell_output("#{bin}/sym version")
   end
 end
-
